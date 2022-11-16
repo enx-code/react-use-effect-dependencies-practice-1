6c3f281e3cc3d7664d11e2a46e2ec4618416c726
Enky <enkh6at@yahoo.com>
2022-11-16 12:07:55 +0000

    complete

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import DataList from "./components/DataList";
 import SelectTypeForm from "./components/SelectTypeForm";
 import "./styles.css";
 
+//App.js
 export default function App() {
   const [dataType, setDataType] = useState("");
 
   const [data, setData] = useState(null);
 
   console.log({ data });
+  console.log(dataType);
 
   // Write code here.
-  //
+  useEffect(() => {
+    if (dataType === "") {
+      setData(null);
+      return;
+    }
+    fetch(`https://swapi-new.herokuapp.com/api/${dataType}/`)
+      .then((res) => res.json())
+      .then((apiData) => setData(apiData));
+  }, [dataType]);
 
   return (
     <div>
--- src/components/SelectTypeForm.js
+++ src/components/SelectTypeForm.js
@@ -1,10 +1,15 @@
-function SelectTypeForm(props) {
+//SelectTypeForm.js
+export default function SelectTypeForm(props) {
   // Write code here...
+
+  const handleChange = (event) => {
+    props.setDataType(event.target.value);
+  };
 
   return (
     <form className="three-column-grid__expand-two gap-md">
       <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
+      <select id="type" name="type" onChange={handleChange}>
         <option value="">Please select...</option>
         <option value="people">People</option>
         <option value="planets">Planets</option>
@@ -13,5 +18,3 @@ function SelectTypeForm(props) {
     </form>
   );
 }
-
-export default SelectTypeForm;
